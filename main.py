b17fd56d6609433053e4f70ac606741d90d3e157
Maxon07TEH <maksbutenko78@gmail.com>
2024-09-12 17:02:18 +0300

    щк

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ while True:
         webbrowser.open("https://www.youtube.com/@thenevermindslol")
     if response == "Niko?":
         print("нашла, открываю...")
-        os.startfile("Content.pictute.OneShot\.Niko\.niko.png")
+        os.startfile("Content.pictute.OneShot/.Niko/.niko.png")
     if response == "OneShot?":
         print("нашла, открываю...")
         webbrowser.open("https://www.youtube.com/@thenevermindslol")
@@ -21,16 +21,23 @@ while True:
         webbrowser.open("https://www.youtube.com/watch?v=KzxORyneqNk&list=PLdD5TVTzatJDQAwrXIQ45b0NsiFpSjr0a&index=17&pp=iAQB8AUB")
     if response == "aviator":
         print("нашла, открываю...")
-        os.system("start Content.song.ogg/Aviator.ogg")
+        os.startfile("Content.song.ogg/Aviator.ogg")
+    if response == "motivation pls":
+        print("нашла, открываю...")
+        os.startfile("Content.Video.mp4\Motivatoin.mp4")
+    if response == "Материнская плата asus ROG strix B550-XE gaming wi":
+        print("обама! задарите ее глебу пж")
     if response == "Happy!":
         print("нашла, открываю...")
-        os.startfile("Content.Video.mp4\HappyHiko!.mp4")
+        os.startfile("Content.Video.mp4/HappyHiko!.mp4")
     if response == "Find.OneShot.r34":
         print("я тебя ненавижу, иди нахуй")
+    if response == "Makar?":
+        print("макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор макар пидор ")
     if response == "Find.OneShot.r34.IIgnoreYouHate":
         if input("ты реально уверен? Ты точно этого хочешь? Тебя будун ненавидеть! Ты даже меня будешь ненавидеть! ТЫ НЕ ХОЧЕШЬ ЭТО ВИДЕТЬ!!! ИЗВРАЩЮГА!!!!! TWM HATE YOU!!!!") == "да":
             print("я тебя ненавижу, но ладно")
-            os.startfile("Content.pictute.OneShot\Memes\I HATE R34.jpg")
+            os.startfile("Content.pictute.OneShot/Memes/I HATE R34.jpg")
         else:
             print("молодец, я знала, что ты шутишь!")
 
