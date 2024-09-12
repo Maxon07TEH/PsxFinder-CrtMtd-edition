d1833cc664dde097e5462a9e8d727a4e87b08319
Maxon07TEH <maksbutenko78@gmail.com>
2024-09-12 13:38:54 +0300

    update

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import webbrowser
+import sys
+
+while True:
+    response = input("узнать о...")
+    if response == "nevermindslol":
+        print("нашла, открываю...")
+        webbrowser.open("https://www.youtube.com/@thenevermindslol")
+    if response == "corrupt":
+        print("нашла, открываю...")
+        webbrowser.open("https://www.youtube.com/@thenevermindslol")
+    if response == "Niko?":
+        print("нашла, открываю...")
+        os.startfile("Content.pictute.OneShot\.Niko\.niko.png")
+    if response == "OneShot?":
+        print("нашла, открываю...")
+        webbrowser.open("https://www.youtube.com/@thenevermindslol")
+    if response == "TowardTheSun":
+        print("нашла, открываю...")
+        webbrowser.open("https://www.youtube.com/watch?v=KzxORyneqNk&list=PLdD5TVTzatJDQAwrXIQ45b0NsiFpSjr0a&index=17&pp=iAQB8AUB")
+    if response == "aviator":
+        print("нашла, открываю...")
+        os.system("start Content.song.ogg/Aviator.ogg")
+    if response == "Happy!":
+        print("нашла, открываю...")
+        os.startfile("Content.Video.mp4\HappyHiko!.mp4")
+    if response == "Find.OneShot.r34":
+        print("я тебя ненавижу, иди нахуй")
+    if response == "Find.OneShot.r34.IIgnoreYouHate":
+        if input("ты реально уверен? Ты точно этого хочешь? Тебя будун ненавидеть! Ты даже меня будешь ненавидеть! ТЫ НЕ ХОЧЕШЬ ЭТО ВИДЕТЬ!!! ИЗВРАЩЮГА!!!!! TWM HATE YOU!!!!") == "да":
+            print("я тебя ненавижу, но ладно")
+            os.startfile("Content.pictute.OneShot\Memes\I HATE R34.jpg")
+        else:
+            print("молодец, я знала, что ты шутишь!")
+
+    response = ""
